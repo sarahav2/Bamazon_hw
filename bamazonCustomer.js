0f1c8023e497791665a00bceb11116f625087dd1
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-16 09:40:13 -0500

    added prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,47 @@ var connection = mysql.createConnection({
     password: "",
     database: "Bamazon"
   })
+
+  function start(){
+    //prints the items for sale and their details
+    connection.query('SELECT * FROM Products', function(err, res){
+      if(err) throw err;
+    
+      console.log('_.~"~._.~"~._.~Welcome to BAMazon~._.~"~._.~"~._')
+      console.log('----------------------------------------------------------------------------------------------------')
+    
+      for(var i = 0; i<res.length;i++){
+        console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
+        console.log('--------------------------------------------------------------------------------------------------')
+      }
+      
+      console.log(' ');
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "What is the ID of the product you would like to purchase?",
+          validate: function(value){
+            if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+              return true;
+            } else{
+              return false;
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "qty",
+          message: "How much would you like to purchase?",
+          validate: function(value){
+            if(isNaN(value)){
+              return false;
+            } else{
+              return true;
+            }
+          }
+        }
+        ]).then(function(ans){
+          var whatToBuy = (ans.id)-1;
+          var howMuchToBuy = parseInt(ans.qty);
+          var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
