a9572d71875414b86ba30a48f98a9719422f5937
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-16 09:42:18 -0500

    able to check quantity is sufficient

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,7 +22,7 @@ var connection = mysql.createConnection({
         console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
         console.log('--------------------------------------------------------------------------------------------------')
       }
-      
+
       console.log(' ');
       inquirer.prompt([
         {
@@ -53,3 +53,42 @@ var connection = mysql.createConnection({
           var whatToBuy = (ans.id)-1;
           var howMuchToBuy = parseInt(ans.qty);
           var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
+
+        //check if quantity is sufficient
+      if(res[whatToBuy].StockQuantity >= howMuchToBuy){
+        //after purchase, updates quantity in Products
+        connection.query("UPDATE Products SET ? WHERE ?", [
+        {StockQuantity: (res[whatToBuy].StockQuantity - howMuchToBuy)},
+        {ItemID: ans.id}
+        ], function(err, result){
+            if(err) throw err;
+            console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+        });
+
+        connection.query("SELECT * FROM Departments", function(err, deptRes){
+          if(err) throw err;
+          var index;
+          for(var i = 0; i < deptRes.length; i++){
+            if(deptRes[i].DepartmentName === res[whatToBuy].DepartmentName){
+              index = i;
+            }
+          }
+          
+          //updates totalSales in departments table
+          connection.query("UPDATE Departments SET ? WHERE ?", [
+          {TotalSales: deptRes[index].TotalSales + grandTotal},
+          {DepartmentName: res[whatToBuy].DepartmentName}
+          ], function(err, deptRes){
+              if(err) throw err;
+              //console.log("Updated Dept Sales.");
+          });
+        });
+
+      } else{
+        console.log("Sorry, there's not enough in stock!");
+      }
+
+      reprompt();
+    })
+})
+}
